2a9bf030943520edb8540bf8ba011a39dc65691c
theKnightOPS <sk9355622@gmail.com>
2023-05-26 20:36:45 +0530

    created codre and user app

---
+++ app/core/models.py
@@ -0,0 +1,61 @@
+"""
+Django custom user model
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, mobile, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have an email address.")
+        if not mobile:
+            raise ValueError("User must have a mobile number.")
+        user = self.model(email=self.normalize_email(email), mobile=mobile,
+                          **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, mobile, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, mobile, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    mobile = models.IntegerField(max_length=255, blank=False, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = ["mobile"]
+
+    def __str__(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
